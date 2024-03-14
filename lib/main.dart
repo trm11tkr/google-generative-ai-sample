13a878b6f61b3a16ae35c066a61c50b17ee17491
trm11tkr <mrmrt1121@gmail.com>
2024-03-14 22:23:48 +0900

    fix: Wrap MainApp by ProviderScope

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:google_generative_ai_sample/features/chat/chat_page.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void main() async {
   await dotenv.load(fileName: ".env");
-  runApp(const MainApp());
+  runApp(const ProviderScope(child: MainApp()));
 }
 
 class MainApp extends StatelessWidget {
